16cfd0c56f57aee38cc9e03d5dc6f4b94001255f
Fayez Shahid <fayezshahid167@gmail.com>
2022-12-07 09:26:31 +0500

    background image

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>PHOTON</title>
 </head>
-<body class="bg-light align-items-center">
+<body class="bg-light align-items-center" style="background-image: url('background.jpeg'); background-repeat: no-repeat; background-size: cover;">
     <div class="container">
         <div class="row mt-5">
             <div class="col-lg-4 bg-white m-auto rounded-top wrapper">
